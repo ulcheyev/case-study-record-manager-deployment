606855c2d52b1a8ae1ef9870e30ca31ba2bd7c91
Yevgeniy <ulchenkov2001@gmail.com>
2026-03-25 17:11:34 +0100

    [upd] media cms dev profiles

--- configs/mediacms/local_settings.py
+++ configs/mediacms/local_settings.py
@@ -2,13 +2,8 @@ import os
 from .settings import *
 import time
 
-# ========================
-# BASE
-# ========================
-
 FRONTEND_HOST = os.getenv('FRONTEND_HOST', 'http://localhost')
 PORTAL_NAME = os.getenv('PORTAL_NAME', 'MediaCMS')
-
 REDIS_LOCATION = os.getenv('REDIS_LOCATION', 'redis://redis:6379/1')
 
 DATABASES = {
@@ -33,25 +28,24 @@ CACHES = {
     }
 }
 
-# ========================
-# CELERY
-# ========================
-BROKER_URL = REDIS_LOCATION
-CELERY_RESULT_BACKEND = BROKER_URL
+# OID
+USE_IDENTITY_PROVIDERS = True
+USE_RBAC = True
+INSTALLED_APPS = INSTALLED_APPS + [
+    "allauth.socialaccount.providers.openid_connect",
+]
 
-# ========================
-# VIDEO
-# ========================
-DO_NOT_TRANSCODE_VIDEO = os.getenv('DO_NOT_TRANSCODE_VIDEO', 'False') == 'True'
 
-# ========================
-# SECRET LOADER
-# ========================
 def read_secret(
         path="/secrets/mediacms_client_secret",
-        timeout=1000,
-        interval=5,
+        timeout=2000,
+        interval=10,
 ):
+    """
+    Wait until secret file appears.
+    Fails after timeout seconds.
+    """
+
     start = time.time()
 
     while True:
@@ -59,25 +53,59 @@ def read_secret(
             with open(path) as f:
                 secret = f.read().strip()
                 if secret:
-                    print("[OIDC] Secret loaded")
+                    print("Secrets injected...")
                     return secret
 
         if time.time() - start > timeout:
-            print(f"[OIDC] Secret not found after {timeout}s: {path}")
-            sys.exit(1)
+            raise RuntimeError(
+                f"Secret file not found after {timeout} seconds: {path}"
+            )
+        # TODO exit 1
 
         time.sleep(interval)
 
-# ========================
-# PROFILE LOADER
-# ========================
-PROFILE = os.getenv("DJANGO_PROFILE", "prod")
+print("Waiting for OIDC secret...")
+OIDC_SECRET = read_secret()
+REGISTER_ALLOWED = False
 
-print(f"[Settings] Loading profile: {PROFILE}")
+SOCIALACCOUNT_PROVIDERS = {
+    "openid_connect": {
+        "APPS": [
+            {
+                "provider_id": "keycloak",
+                "name": "Keycloak",
+                "client_id": "mediacms",
+                "secret": OIDC_SECRET,
+                "settings": {
+                    "server_url": os.getenv(
+                        "OIDC_SERVER_URL",
+                        "http://auth-server:8080/realms/record-manager/.well-known/openid-configuration",
+                    ),
+                },
+            }
+        ]
+    }
+}
 
-if PROFILE == "dev":
-    from .profiles.dev import *
-elif PROFILE == "prod":
-    from .profiles.prod import *
-else:
-    raise RuntimeError(f"Unknown DJANGO_PROFILE: {PROFILE}")
+
+MIDDLEWARE = MIDDLEWARE + [
+    'deploy.docker.protected_media.ProtectedMediaMiddleware',
+]
+
+GLOBAL_LOGIN_REQUIRED = True
+LOGIN_URL = "/accounts/oidc/keycloak/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/oidc/keycloak/login/"
+
+SOCIALACCOUNT_ADAPTER = "deploy.docker.oidc_adapter.RoleRestrictedSocialAccountAdapter"
+MEDIACMS_REQUIRED_ROLE = os.getenv(
+    "MEDIACMS_REQUIRED_ROLE",
+    "mediacms-access-role"
+)
+
+# CELERY STUFF
+BROKER_URL = REDIS_LOCATION
+CELERY_RESULT_BACKEND = BROKER_URL
+DO_NOT_TRANSCODE_VIDEO = os.getenv('DO_NOT_TRANSCODE_VIDEO', 'False') == 'True'
+MP4HLS_COMMAND = "/home/mediacms.io/bento4/bin/mp4hls"
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
