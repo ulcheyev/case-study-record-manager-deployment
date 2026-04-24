05f789728336c77c689da3107fb7aa0ad39bff61
Yevgeniy <ulchenkov2001@gmail.com>
2026-04-24 21:55:28 +0200

    [fix] customization of keycloak

---
+++ configs/keycloak-config/customization/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    keycloak = {
+      source  = "keycloak/keycloak"
+      version = ">= 5.0.0"
+    }
+  }
+}
+
+provider "keycloak" {
+  client_id = "admin-cli"
+  username  = var.kc_admin_user
+  password  = var.kc_admin_password
+  url       = var.kc_url
+}
